d30be499177f691a13c22c08a835db399c61621b
Danielo Rodriguez <rdanielo@gmail.com>
2019-12-13 15:18:22 +0100

    feat: complete python implementationn

--- ls-tree/python/main.py
+++ ls-tree/python/main.py
@@ -2,11 +2,12 @@ import os
 from os import listdir
 from os.path import isdir,join
 
-middle  = '|-'
-pipe    = '|'
-last    = '` '
+middle  = '├'
+pipe    = '│'
+last    = '└'
+scale = 2
 
-def traverse(path, parents='', depth=0, isLast=False):
+def traverse(path, parents='', depth=0, isLast=True):
     tree = [(path, parents, depth, isLast)]
     realPath = join(parents,path)
     files = listdir(realPath)
@@ -17,17 +18,26 @@ def traverse(path, parents='', depth=0, isLast=False):
         if isdir(curPath):
             tree = tree + (traverse(file,realPath, depth+1, isLast))
         else:
-            tree.append((file, parents, depth, isLast))
+            tree.append((file, parents, depth+1, isLast))
     return tree
 
-def addDepth(depth,spacer=" "):
-    return "".join([spacer for x in range(0, depth)])
+def addDepth(depth,connections,spacer=" "):
+    return "".join([pipe if x in connections else spacer for x in range(0, depth)])
+
+def findConnections(depth, below):
+    depths = []
+    for (_,_,curDepth,_) in below:
+        if curDepth < depth: 
+            depths.append(curDepth)
+            depth=curDepth
+    return depths
 
 def prettyPrint(treeInfo):
-    for node in treeInfo:
+    for idx,node in enumerate(treeInfo):
         (name, parents, depth, isLast) = node
         prefix = last if isLast else middle
-        print("%s %s %s"%(addDepth(depth), prefix, name))
+        connections = [x*scale for x in findConnections(depth,treeInfo[idx:])]
+        print("%s%s %s"%(addDepth(depth*scale,connections), prefix, name))
 
 folderInfo = traverse(".")
 print(prettyPrint(folderInfo))
