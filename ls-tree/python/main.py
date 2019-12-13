d73b90c9c00e8270242a1392fa4d524df2f3f21c
Danielo Rodriguez <rdanielo@gmail.com>
2019-12-13 14:04:13 +0100

    feat: python getting closer

--- ls-tree/python/main.py
+++ ls-tree/python/main.py
@@ -2,12 +2,12 @@ import os
 from os import listdir
 from os.path import isdir,join
 
-middle  = '-'
+middle  = '|-'
 pipe    = '|'
-last    = '`'
+last    = '` '
 
-def traverse(path, parents=''):
-    tree = [path]
+def traverse(path, parents='', depth=0, isLast=False):
+    tree = [(path, parents, depth, isLast)]
     realPath = join(parents,path)
     files = listdir(realPath)
     maxFiles = len(files)
@@ -15,9 +15,19 @@ def traverse(path, parents=''):
         curPath = join(realPath,file)
         isLast = idx == maxFiles -1
         if isdir(curPath):
-            tree.append(traverse(file,realPath))
+            tree = tree + (traverse(file,realPath, depth+1, isLast))
         else:
-            tree.append(file)
+            tree.append((file, parents, depth, isLast))
     return tree
 
-print(traverse("."))
+def addDepth(depth,spacer=" "):
+    return "".join([spacer for x in range(0, depth)])
+
+def prettyPrint(treeInfo):
+    for node in treeInfo:
+        (name, parents, depth, isLast) = node
+        prefix = last if isLast else middle
+        print("%s %s %s"%(addDepth(depth), prefix, name))
+
+folderInfo = traverse(".")
+print(prettyPrint(folderInfo))
