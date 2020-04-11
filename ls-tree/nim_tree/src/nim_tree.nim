9cc6f09d9b866e9ece598345532a44774acd00f7
Danielo Rodriguez <rdanielo@gmail.com>
2020-04-11 15:43:52 +0200

    feat: implemented spacing

--- ls-tree/nim_tree/src/nim_tree.nim
+++ ls-tree/nim_tree/src/nim_tree.nim
@@ -6,32 +6,46 @@ type
   FileInfo = object
     name*: string
     depth*: int
+type
+  Config = object
+    space*: string
+    middle*: string
+    pipe*: string
+    last*: string
 
-proc traverse_dir(dir:string): seq[FileInfo] =
-  for path in walkDirRec(dir):
-    var depth = 0
-    for _ in path.parentDirs: depth += 1
+proc traverse_dir(dir:string, depth=0): seq[FileInfo] =
+  for kind, path in walkDir(dir):
     result.add FileInfo(name:path.splitPath.tail, depth:depth) 
+    if kind == pcDir:
+      result = result.concat traverse_dir(path, depth+1)
+
 
 iterator items*[T](s: Slice[T]): T =
   for i in s.a .. s.b:
     yield i
   
-proc printFile(file:FileInfo, idx: int, files: seq[FileInfo]) =
+proc printFile(file:FileInfo, idx: int, files: seq[FileInfo], conf: Config) =
   var depth = file.depth
-  var row = newSeqWith(depth, " ")
+  var row = newSeqWith(depth, conf.space)
   for pos in idx..<files.len:
     let f = files[pos]
     if f.depth < depth:
       depth = f.depth
-      row[depth] = "|"
+      row[depth] = conf.pipe
 
-  echo(row.join, "|-", file.name, file.depth)
+  echo(row.join, conf.middle, file.name)
   
 
 when isMainModule:
   let dir = "../"
   echo(dir)
   let files = traverse_dir(dir)
+  let spacing = 4
+  let conf = Config(
+    space: " ".repeat(spacing),
+    middle: "├".indent(spacing),
+    pipe: "│".indent(spacing),
+    last: "└".indent(spacing))
+
   for idx, file in files:
-    file.printFile(idx, files)
+    file.printFile(idx, files,conf)
